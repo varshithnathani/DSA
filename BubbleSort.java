086a906c150335790e3d18012e1c627bac3865cf
Nathani Varshith <123011598+varshithnathani@users.noreply.github.com>
2024-03-21 00:35:18 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,39 @@
+// Bubble Sort ==> It iterate to the array and arrange them in the sequence order
+
+
+import java.util.*;
+class BubbleSort{
+    public static void main(String[] args){
+        // Scanner for user input
+        Scanner SC = new Scanner(System.in);
+        System.out.println("Please Enter the Size of the Array :");
+        // n for size of the Array
+        int n = SC.nextInt();
+        System.out.println("Please Enter the Array :");
+        int[] arr = new int[n];
+        // Taking the Array details
+        for(int i =0; i<n; i++){
+            arr[i] = SC.nextInt();
+        }
+        bubbleSort(arr);;
+
+    }
+
+    public static void bubbleSort(int[] arr){
+        for(int i =0; i<arr.length-1; i++){
+            for(int j =0; j<arr.length-1-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        display(arr);
+
+    }
+    public static void display(int[] arr){
+        System.out.println(Arrays.toString(arr));
+    }
+}
